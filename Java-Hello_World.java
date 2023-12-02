17b934aaa87c0a5f157399dd8ca3666056341dbe
Delio Labaddan <149133144+DellPogie@users.noreply.github.com>
2023-12-02 14:22:44 +0800

    Add files via upload

---
+++ Java-Hello_World.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("ooooo   ooooo           oooo  oooo                   oooooo   oooooo     oooo                    oooo        .o8  .o. ");
+		System.out.println("`888'   `888'           `888  `888                    `888.    `888.     .8'                     `888       '888  888 ");
+		System.out.println(" 888     888   .ooooo.   888   888   .ooooo.           `888.   .8888.   .8'    .ooooo.  oooo d8b  888   .oooo888  888 ");
+		System.out.println(" 888ooooo888  d88' `88b  888   888  d88' `88b           `888  .8'`888. .8'    d88' `88b `888''8P  888  d88' `888  Y8P ");
+		System.out.println(" 888     888  888ooo888  888   888  888   888            `888.8'  `888.8'     888   888  888      888  888   888  `8' ");
+		System.out.println(" 888     888  888    .o  888   888  888   888 .o.         `888'    `888'      888   888  888      888  888   888  .o. ");
+		System.out.println("o888o   o888o `Y8bod8P' o888o o888o `Y8bod8P' Y8P          `8'      `8'       `Y8bod8P' d888b    o888o `Y8bod88P' Y8P ");
+	}
+}
